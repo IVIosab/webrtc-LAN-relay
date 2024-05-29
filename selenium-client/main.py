3d5055baa960bac63c384fdd8f8da6fbc1199d3a
Mosab Mohamed <mosab.f.r@gmail.com>
2024-05-29 22:22:04 +0300

    add: cpu and mem data collection

--- selenium-client/main.py
+++ selenium-client/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import sys
 import signal
 import time
+import subprocess
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,7 +11,6 @@ from selenium.webdriver.common.by import By
 
 def main(
     relay: bool,
-    headless: bool,
     url: str,
     windows: int,
     time_test: int,
@@ -22,8 +22,9 @@ def main(
     options.add_argument("ignore-certificate-errors")
     options.add_argument("no-sandbox")
     options.add_argument("disable-dev-shm-usage")
-    if headless:
-        options.add_argument("--headless=new")
+    options.add_argument("site-per-process")
+    # options.add_argument("--process-per-site")
+    # options.add_argument("--incognito")
 
     driver = Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
@@ -33,55 +34,71 @@ def main(
 
     signal.signal(signal.SIGTERM, onexit)
 
+    subprocesses = []
+
     try:
         driver.get(url="chrome://webrtc-internals/")
-
+        driver.implicitly_wait(5)
         for i in range(windows):
             driver.switch_to.new_window("window")
             driver.get(url=url)
             driver.implicitly_wait(5)
             try:
-                ngrok_button = driver.find_element(
-                    By.XPATH, '//button[text()="Visit Site"]'
-                )
-                ngrok_button.click()
+                driver.find_element(By.XPATH, '//button[text()="Visit Site"]').click()
                 driver.implicitly_wait(5)
             except:
                 pass
-
-        print("Initialization Finished")
 
         if relay:
             driver.switch_to.window(driver.window_handles[1])
             driver.implicitly_wait(5)
 
-            relay_button = driver.find_element(By.XPATH, '//button[text()="Relay"]')
-            relay_button.click()
+            driver.find_element(By.XPATH, '//button[text()="Relay"]').click()
             driver.implicitly_wait(5)
 
         driver.switch_to.window(driver.window_handles[0])
         driver.implicitly_wait(5)
+        pids = driver.find_elements(
+            By.XPATH, "//div[contains(text(), 'Caller process id:')]"
+        )
+        pids = [pid.text.split(":")[1].strip() for pid in pids]
+        print(f"pids: {pids}")
 
         time.sleep(60)
+        print("Initialization Finished")
+
         driver.refresh()
         driver.implicitly_wait(5)
 
         idx = 1
         while True:
             try:
-                span = driver.find_element(By.XPATH, f"/html/body/p/div[1]/span[{idx}]")
-                span.click()
-                driver.implicitly_wait(5)
+                driver.find_element(
+                    By.XPATH, f"/html/body/p/div[1]/span[{idx}]"
+                ).click()
                 idx += 1
+                driver.implicitly_wait(5)
             except:
                 break
 
-        time.sleep(time_test)
+        # Open subprocess for each pid
+        # for pid in pids:
+        #     cmd = f'sudo atop -J PRM,PRC,PRN 1 | sed \'s/"cgroup": "[^"]*"/"cgroup": ""/g\' | jq --unbuffered -c \'{{ timestamp: .timestamp, net: (.PRN[] | select(.pid == {pid})), cpu: (.PRC[] | select(.pid == {pid})), mem: (.PRM[] | select(.pid == {pid}))}}\''
+        #     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, text=True)
+        #     subprocesses.append((proc, f"{pid}.json"))
 
-        x = driver.find_element(By.XPATH, "/html/body/p/details[1]")
-        x.click()
-        x = driver.find_element(By.XPATH, "/html/body/p/details[1]/div/div/a/button")
-        x.click()
+        time.sleep(time_test)
+        # Close subprocesses
+        driver.find_element(By.XPATH, "/html/body/p/details[1]").click()
+        driver.find_element(
+            By.XPATH, "/html/body/p/details[1]/div/div/a/button"
+        ).click()
+
+        # Save output and close subprocesses
+        # for proc, filename in subprocesses:
+        #     with open(filename, "w") as file:
+        #         file.write(proc.stdout.read())
+        #     proc.terminate()
 
         time.sleep(10)
     finally:
@@ -92,14 +109,12 @@ def main(
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--relay", action="store_true")
-    parser.add_argument("--headless", action="store_true")
     parser.add_argument("--url", action="store")
     parser.add_argument("--windows", action="store", default=2)
     parser.add_argument("--time", action="store", default=300)
     args = parser.parse_args()
     main(
         args.relay,
-        args.headless,
         f"https://{args.url}.ngrok-free.app/",
         int(args.windows),
         int(args.time),
