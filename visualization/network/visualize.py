3a3af63e16ad5c04c985696c49a769a8b17951ec
Mosab Mohamed <mosab.f.r@gmail.com>
2024-05-31 02:51:52 +0300

    add: rewrite parsing and visualization scripts

---
+++ visualization/network/visualize.py
@@ -0,0 +1,98 @@
+import os
+import csv
+import matplotlib.pyplot as plt
+
+
+def read_csv(file_path):
+    data_dict = {}
+    with open(file_path, "r") as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            peer = row["Peer"]
+            data_dict[peer] = {
+                "bytes_sent": [],
+                "bytes_sent_in_bits/s": [],
+                "bytes_received": [],
+                "bytes_received_in_bits/s": [],
+            }
+            data_dict[peer]["bytes_sent"] = eval(row["BytesSent"])
+            data_dict[peer]["bytes_sent_in_bits/s"] = eval(row["BytesSent_in_bits/s"])
+            data_dict[peer]["bytes_received"] = eval(row["BytesReceived"])
+            data_dict[peer]["bytes_received_in_bits/s"] = eval(
+                row["BytesReceived_in_bits/s"]
+            )
+    return data_dict
+
+
+def fill_list(list, size):
+    zeros = [0] * (size - len(list))
+    if len(zeros) > 0:
+        list = zeros + list
+    return list
+
+
+def get_max_length(data_dict):
+    max_length = 0
+    for peer, peer_data in data_dict.items():
+        for metric, metric_data in peer_data.items():
+            if len(metric_data) > max_length:
+                max_length = len(metric_data)
+    return max_length
+
+
+def normalize_data(data_dict):
+    max_length = get_max_length(data_dict)
+    for peer, peer_data in data_dict.items():
+        for metric, metric_data in peer_data.items():
+            data_dict[peer][metric] = fill_list(metric_data, max_length)
+    return data_dict, max_length
+
+
+def get_time(data_dict, length):
+    diff = length - len(data_dict["bytes_sent_in_bits/s"])
+    return list(range(diff, length))
+
+
+def plot_data(data_dict, output_file):
+    length = get_max_length(data_dict)
+
+    colors = {}
+    plt.figure(figsize=(20, 10))
+    for idx, (peer, peer_data) in enumerate(data_dict.items()):
+        time = get_time(peer_data, length)
+        if peer not in colors:
+            colors[peer] = plt.cm.tab10(idx % 10)
+        color = colors[peer]
+        plt.plot(
+            time,
+            peer_data["bytes_sent_in_bits/s"],
+            label=f"{peer} sent",
+            linestyle="--",
+            color=color,
+        )
+        plt.plot(
+            time,
+            peer_data["bytes_received_in_bits/s"],
+            label=f"{peer} received",
+            linestyle="-",
+            color=color,
+        )
+    plt.xlabel("Time (s)")
+    plt.ylabel("Bits/s")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(output_file)
+    plt.close()
+
+
+def main(input_dir="parsed_input", output_dir="figures"):
+    for file_name in os.listdir(input_dir):
+        if file_name.endswith(".csv"):
+            data_dict = read_csv(os.path.join(input_dir, file_name))
+            plot_data(
+                data_dict, os.path.join(output_dir, file_name.replace(".csv", ".png"))
+            )
+
+
+if __name__ == "__main__":
+    main()
